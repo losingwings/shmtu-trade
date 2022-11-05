0f567067cca510679f2910a6c7a203fa23d85c6f
wings <108850490+losingwings@users.noreply.github.com>
2022-11-05 21:08:33 +0800

    Add files via upload

---
+++ src/main/java/com/school/springboot/controller/dto/UserPasswordDTO.java
@@ -0,0 +1,11 @@
+package com.school.springboot.controller.dto;
+
+import lombok.Data;
+
+@Data
+public class UserPasswordDTO {
+    private String username;
+    private String phone;
+    private String password;
+    private String newPassword;
+}
