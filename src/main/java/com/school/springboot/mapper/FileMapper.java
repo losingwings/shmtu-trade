b9d91c0c82e17976211a9de50ef1c93e756d93c2
wings <108850490+losingwings@users.noreply.github.com>
2022-11-05 21:12:22 +0800

    Delete src/main/java/com/school/springboot/mapper directory

--- src/main/java/com/school/springboot/mapper/FileMapper.java
+++
@@ -1,7 +0,0 @@
-package com.school.springboot.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.school.springboot.entity.Files;
-
-public interface FileMapper extends BaseMapper<Files> {
-}
